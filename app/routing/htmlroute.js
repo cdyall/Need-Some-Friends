678e6b13406ad1e5ec3bca0b770a818839c12c54
cdyall <cdyall1@gmail.com>
2019-08-02 16:10:15 -0400

    upload2

--- app/routing/htmlroute.js
+++ app/routing/htmlroute.js
@@ -1,16 +1,25 @@
-//Dependencies
+console.log('HTML Route Connected Successfully');
+
+
+// Node Dependencies
 var path = require('path');
 
-//ROUTING
 
-module.exports = function(app){
-  //default GET route that leads to home.html - displays home page
+// Includes Two Routes
+function htmlRoutes(app) {
+
+  // A GET Route to /survey which should display the survey page.
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-  //a USE route to home page
+  // A default USE route that leads to home.html which displays the home page.
   app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
-};
+
+}
+
+
+// Export for use in main server.js file
+module.exports = htmlRoutes;
