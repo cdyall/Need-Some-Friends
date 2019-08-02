c6c10b37e82404b83c6a22955bf6145e569722e5
cdyall <46546078+cdyall@users.noreply.github.com>
2019-08-02 13:04:02 -0400

    Add files via upload

---
+++ app/data/friend.js
@@ -0,0 +1,23 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Chris Dyall',
+      profilePic: 'https://avatars1.githubusercontent.com/u/46546078?s=400&v=4',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4",
+        "3"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
