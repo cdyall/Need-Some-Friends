ceadd577fac726db4917b754fd5cc47f6de48549
cdyall <cdyall1@gmail.com>
2019-08-19 18:36:19 -0400

    upload

--- app/routing/htmlroute.js
+++ app/routing/htmlroute.js
@@ -1,25 +1,11 @@
-console.log('HTML Route Connected Successfully');
+const path = require("path");
 
-
-// Node Dependencies
-var path = require('path');
-
-
-// Includes Two Routes
-function htmlRoutes(app) {
-
-  // A GET Route to /survey which should display the survey page.
-  app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+module.exports = function(app) {
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
   });
 
-  // A default USE route that leads to home.html which displays the home page.
-  app.use(function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
-
-}
-
-
-// Export for use in main server.js file
-module.exports = htmlRoutes;
+};
--- server.js
+++ server.js
@@ -1,37 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-// Node Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-
-
-// Link in html and api routes
-var apiRoutes = require('./app/routing/api-routes.js');
-var htmlRoutes = require('./app/routing/html-routes.js');
-
-
-
-// Set up Express App
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-
-
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 
-// Server Routing Map 
-apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
-htmlRoutes(app); // HTML route 
-
-
-// Listener - Start the server
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(port, () => console.log("Listening on port %s", port));
